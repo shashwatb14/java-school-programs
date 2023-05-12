47542429f84530e521bfbdd3d853a5137e96514a
Shashwat <shashwat.bhandari.14@gmail.com>
2023-05-12 09:11:21 +0800

    Add hasNext() and empty constructor

--- ADTs/Node.java
+++ ADTs/Node.java
@@ -2,9 +2,16 @@ public class Node {
     int data;
     Node next; // self-referential
 
+    // empty constructor
+    public Node() {}
+
     // constructor for initializing a node
     public Node(int d) {
         this.data = d;
+    }
+
+    public boolean hasNext() {
+        return this.next != null;
     }
 
     public String toString() {
