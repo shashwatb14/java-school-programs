d46ab798e728b7822816e1ed787b295e0e140d04
Shashwat <shashwat.bhandari.14@gmail.com>
2023-05-12 10:14:15 +0800

    Update linked list

--- ADTs/LinkedList.java
+++ ADTs/LinkedList.java
@@ -1,6 +1,9 @@
 public class LinkedList {
     Node start;
 
+    // empty constructor
+    public LinkedList() {}
+
     public LinkedList(int data) {
         Node newNode = new Node(data);
         newNode.next = null;
@@ -13,14 +16,51 @@ public class LinkedList {
 
     public void append(int data) {
         Node newNode = new Node(data);
+
         if (isEmpty()) {
             start = newNode;
+            return;
         }
 
         Node temp = start;
-        while (temp.hasNext()) {
+        while (temp.hasNext()) { // (temp.next != null)
             temp = temp.next;
         }
         temp.next = newNode;
+    }
+
+    public boolean delete(int dataToDelete) {
+        if (isEmpty()) {
+            System.out.println("Error - linked list is empty.");
+            return false;
+        }
+
+        if (start.data == dataToDelete) {
+            start = start.next;
+            return true;
+        }
+
+        Node prev = start;
+        Node current = start.next;
+        while(current != null) {
+            if (current.data == dataToDelete) {
+                prev.next = current.next;
+                return true;
+            }
+            prev = prev.next;
+            current = current.next;
+        }
+
+        return false;
+    }
+
+    public void printLinkedList() {
+        Node temp = start;
+        System.out.print("start");
+        while (temp != null) {
+            System.out.print(" -> " + temp.data);
+            temp = temp.next;
+        }
+        System.out.println(" -> null");
     }
 }
--- ADTs/LinkedListDemo.java
+++ ADTs/LinkedListDemo.java
@@ -1,5 +1,20 @@
 public class LinkedListDemo {
     public static void main(String[] args) {
-        
+        LinkedList list = new LinkedList(5);
+        list.append(7);
+        list.append(3);
+        list.printLinkedList();
+
+        LinkedList list2 = new LinkedList();
+        list2.append(1);
+        list2.append(5);
+        list2.append(2);
+        list2.append(7);
+        list2.append(0);
+        list2.printLinkedList();
+
+        list2.delete(1); list2.printLinkedList();
+        list2.delete(2); list2.printLinkedList();
+        list2.delete(0); list2.printLinkedList();
     }
 }
