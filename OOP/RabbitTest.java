482e0800560fb88a696d36fade5fbd80d27beddf
Shashwat <shashwat.bhandari.14@gmail.com>
2023-08-25 15:24:33 +0800

    Update Rabbit

--- OOP/RabbitTest.java
+++ OOP/RabbitTest.java
@@ -1,6 +1,6 @@
-    public class RabbitTest {
+public class RabbitTest {
     public static void main(String[] args) {
-        Rabbit rabbit = new Rabbit("idk", 2012, "White");
+        Rabbit rabbit = new Rabbit("Bunny", 2012, "White");
         System.out.println(rabbit);
         System.out.println(rabbit.getBirthYear());
         rabbit.talk();
