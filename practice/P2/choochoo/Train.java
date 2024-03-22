befd310c55aa64edb2ce55575e40b8d3a91f1cb0
Shashwat <shashwat.bhandari.14@gmail.com>
2024-03-22 10:30:41 +0800

    complete choochoo

--- practice/P2/choochoo/Train.java
+++ practice/P2/choochoo/Train.java
@@ -4,7 +4,7 @@ public class Train
 {
 
     private Engine[] mEngines;
-    private Wagon[] mWagons;
+    private Wagon firstWagon;
     private int mEngineCount;
     private int mWagonCount;
     private int mTrainNumber;
@@ -16,7 +16,7 @@ public class Train
         mTrainNumber = number;
         mEngines = new Engine[6]; // The train can have up to 6 engines
         mEngineCount = 0;
-        mWagons = new Wagon[100]; // The train can have up to 100 wagons
+        // mWagons = new Wagon[100]; // The train can have up to 100 wagons
         mWagonCount = 0;
         mWeight = 0;
     }
@@ -35,26 +35,55 @@ public class Train
 
     public void addWagon(Wagon newWagon)
     {
-        mWagons[mWagonCount] = newWagon;
+        newWagon.setNextWagon(firstWagon);
+        firstWagon = newWagon;
         mWagonCount++;
     }
 
+    // returns the number of wagons currently coupled to the train.
 	public int getNumberOfWagons()
 	{	return this.mWagonCount;
 	}
 
-    public Wagon removeWagon()
-    {	// returns the number of wagons currently coupled to the train.
-		if( this.getNumberOfWagons() > 0 )
-		{	mWagonCount--;
-			return mWagons[mWagonCount];
+    public Wagon removeWagon(int ID)
+    {	
+		/*if( this.getNumberOfWagons() > 0 )
+        {   mWagonCount--;
+			return mWagons[mWagonCount + 1];
 		}
-		return null;
+		return null;*/
+
+        Wagon prevWagon = firstWagon;
+        Wagon currentWagon = firstWagon.getNextWagon();
+        Wagon toReturn = null;
+        while (currentWagon != null) {
+            if (currentWagon.getWagonID() == ID) {
+                toReturn = currentWagon;
+                prevWagon.setNextWagon(currentWagon.getNextWagon());
+            }
+            currentWagon = currentWagon.getNextWagon();
+        }
+
+        return toReturn;
     }
 
     public double getWeight()
     {
-        // Code to be written
+        double weight = 0;
+        for (int i = 0; i < mEngineCount; i++) {
+            weight += mEngines[i].getWeight();
+        }
+        /*for (int i = 0; i < mWagonCount; i++) {
+            weight += mWagons[i].getWeight();
+        }*/
+
+        Wagon tempWagon = firstWagon;
+        while (tempWagon != null) {
+            weight += tempWagon.getWeight();
+            tempWagon = tempWagon.getNextWagon();
+        }
+
+        return weight;
     }
     // ...
 }
--- practice/P2/choochoo/Wagon.java
+++ practice/P2/choochoo/Wagon.java
@@ -1,15 +1,26 @@
 package P2.choochoo;
 
+import java.util.Stack;
+
 public class Wagon extends RollingStock
 {
 
-    private Parcel[] mParcels;
+    public Stack<Parcel> mParcels = new Stack<>();
     private int mParcelCount;
+    private Wagon next;
+
+    public Wagon getNextWagon() {
+        return next;
+    }
+
+    public void setNextWagon(Wagon wagon) {
+        this.next = wagon;
+    }
 
     public Wagon(int ID)
     {
         super(ID, 32000);           // Empty wagon weighs 32000 kilograms
-        mParcels = new Parcel[100];
+        // mParcels = new Parcel[100];
         mParcelCount = 0;
     }
     // Accessor methods
@@ -20,7 +31,29 @@ public class Wagon extends RollingStock
 
     public double getWeight()
     {
-        // Code to be written
+        double weight = super.getWeight();
+        
+        /*for (int i = 0; i < mParcelCount; i++) {
+            weight += mParcels[i].getWeight();
+        }*/
+
+        Stack<Parcel> temp = new Stack<>();
+        temp.addAll(mParcels);
+
+        while (!temp.isEmpty()) {
+            weight += temp.pop().getWeight();
+        }
+        return weight;
+    }
+
+    public void addParcel(Parcel parcel) {
+        mParcels.push(parcel);
+        mParcelCount++;
+    }
+
+    public Parcel getParcel() {
+        mParcelCount--;
+        return mParcels.pop();
     }
     //...
     // Other methods
