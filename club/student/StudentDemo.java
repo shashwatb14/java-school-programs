83abcb2be06b0b332338d117485c55703b3b852e
Shashwat <shashwat.bhandari.14@gmail.com>
2024-04-05 23:20:31 +0800

    Update student

---
+++ club/student/StudentDemo.java
@@ -0,0 +1,11 @@
+package student;
+
+public class StudentDemo {
+    public static void main(String[] args) {
+        String[] subjects = {"Computer Science", "Maths", "Physics", "Digital Societies"};
+        Student shashwat = new Student("Shashwat", 11, subjects);
+        System.out.println(shashwat);
+        System.out.println(shashwat.Check_Subject("Computer Science"));
+        System.out.println(shashwat.Check_Subject("Chemistry"));
+    }
+}
